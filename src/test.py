dd9b267ad792154e93c8606e018c25f03a51af21
Chris Closser <chrisclosser1@gmail.com>
2017-12-18 10:54:13 -0800

    need to ignore environment

--- src/test.py
+++ src/test.py
@@ -4,10 +4,16 @@ import os
 
 def repo_fs():
     """Dir listing of current repo."""
-    for root, dirs, files in os.walk("./"):
-        for file in files:
-            if file.endswith(".py"):
-                print(os.path.join(root, file))
+    for root, dirs, files in os.walk("."):
+        # import pdb; pdb.set_trace()
+        if not dirs.startswith('ENV'):
+            for file in files:
+                if file.endswith(".py"):
+                    print('py file found: ', os.path.join(root, file))
+                if file.endswith(".yml"):
+                    print('.yml file found: ', os.path.join(root, file))
+                if file.endswith(".pip"):
+                    print('pip file found: ', os.path.join(root, file))
 
 
 if __name__ == '__main__':
