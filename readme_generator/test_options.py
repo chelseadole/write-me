455596ca3b335f5ad8bde0f5b9e068b6b7e5877f
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-18 14:43:05 -0800

    adding serving options from within test_options file.

--- readme_generator/test_options.py
+++ readme_generator/test_options.py
@@ -21,3 +21,17 @@ test_options = {
         "run_cov": "`$ coverage report -m`"
     }
 }
+
+serving_options = {
+    "django": {
+        "instructions": "Once you have cloned the application and installed the requirements, you can serve the project on your local machine by executing this command from your terminal, on the same level as `manage.py`:",
+        "serve_command": "`$ ./manage.py runserver`",
+        "hosting": "Once you have executed this command, open your browser, and go to `localhost:8000/`."
+    },
+    "pyramid": {
+        "instructions": "Once you have cloned the application and installed the requirements, you can serve the project on your local machine by executing this command at the root level of your application, at the same level as `development.ini` and `production.ini`.",
+        "serve_command": "`$ pserve development.ini`",
+        "hosting": "Once you have executed this command, open your browser, and go to `localhost:6543/ `."
+    },
+    ""
+}
