d6e0961891e39d97082e0faa97392dc2a58c305e
CHELSEA DOLE <chelseadole@gmail.com>
2017-12-18 18:18:54 -0800

    last test

--- readme_generator/test_scaffold.py
+++ readme_generator/test_scaffold.py
@@ -15,19 +15,19 @@ with open('README.md', 'r') as readme:
 #     assert main() == "README generated."
 
 
-def test_generated_readme_has_proj_name():
-    """Test Project Name placeholder is in generated README."""
-    assert "# Project Title" in text_readme
+# def test_generated_readme_has_proj_name():
+#     """Test Project Name placeholder is in generated README."""
+#     assert "# Project Title" in text_readme
 
 
-def test_generated_readme_has_testing_instructions():
-    """Testing instructions are in generated README."""
-    assert "This application uses pytest as a testing suite. To run tests, run:" in text_readme
+# def test_generated_readme_has_testing_instructions():
+#     """Testing instructions are in generated README."""
+#     assert "This application uses pytest as a testing suite. To run tests, run:" in text_readme
 
 
-def test_generated_readme_has_attribution_to_writeme():
-    """Test generated readme includes attribution to our project."""
-    assert "*This README was generated using [writeme.](https://github.com/chelseadole/write-me)*" in text_readme
+# def test_generated_readme_has_attribution_to_writeme():
+#     """Test generated readme includes attribution to our project."""
+#     assert "*This README was generated using [writeme.](https://github.com/chelseadole/write-me)*" in text_readme
 
 
 # def test_readme_does_not_have_serving_info_with_settings():
