d5c169420f77fb7e2dcf61f5cac60600a4cfdec4
Chris Closser <chrisclosser1@gmail.com>
2017-12-19 15:10:37 -0800

    changed src file

--- write_me/test_tsting_info.py
+++ write_me/test_tsting_info.py
@@ -5,36 +5,33 @@ import pytest
 from tsting_info import get_docstrings
 
 
-@pytest.fixture
-def tfd():
-    """Get test info dict."""
-    return get_docstrings()
+tfd = get_docstrings()
 
 
-def test_non_mpty_dict(tfd):
+def test_non_mpty_dict():
     """Test dictionary is not mpty."""
     assert tfd
 
 
-def test_a_key_in_dict(tfd):
+def test_a_key_in_dict():
     """Test dictionary has file path as keys."""
     tkeys = tfd.keys()
-    assert './src/test_tsting_info.py' in tkeys
+    assert './write_me/test_tsting_info.py' in tkeys
 
 
-def test_a_keys_val_in_dict(tfd):
+def test_a_keys_val_in_dict():
     """Test dictionary has this docstring."""
-    tval = tfd.get('./src/test_tsting_info.py')
+    tval = tfd.get('./write_me/test_tsting_info.py')
     assert 'Test test info dict.' in tval
 
 
-def test_b_key_in_dict(tfd):
+def test_b_key_in_dict():
     """Test dictionary has file path as keys."""
     tkeys = tfd.keys()
-    assert './src/test_list_files.py' in tkeys
+    assert './write_me/test_list_files.py' in tkeys
 
 
-def test_b_keys_val_in_dict(tfd):
+def test_b_keys_val_in_dict():
     """Test dictionary has another docstring."""
-    tval = tfd.get('./src/test_list_files.py')
+    tval = tfd.get('./write_me/test_list_files.py')
     assert 'Test for files listed.' in tval
