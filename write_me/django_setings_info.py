1069b4a27f83a763a7af22c9cbabe0aa86fd9644
Darren-Haynes <darrenhaynes@zoho.com>
2017-12-20 14:34:57 -0800

    get installed apps from django settings.py

--- write_me/django_setings_info.py
+++ write_me/django_setings_info.py
@@ -1,5 +1,7 @@
 """Get django settings."""
 
+import ast
+import re
 
 from list_files import get_settings_files
 
@@ -14,6 +16,14 @@ def get_settings_info():
     for set_file in SETTINGS_FILES:
         docstring = get_docstrings(set_file)
         settings_info[set_file] = docstring
+
+        with open(set_file, 'r') as sf:
+            txt = sf.read()
+            regex = re.compile(r'INSTALLED_APPS = (.*?\])', re.DOTALL)
+            result = re.search(regex, txt)
+            apps_list = ast.literal_eval(re.sub(r'\s*', '', result.group(1)))
+            settings_info['INSTALLED_APPS'] = apps_list
+
     return settings_info
 
 
@@ -21,4 +31,4 @@ if __name__ == '__main__':  # pragma no cover
     res = get_settings_info()
 
     for f in res:
-        print(f, ':',  res[f])
+        print(f, ':', res[f])
