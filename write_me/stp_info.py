bb08d62490d31b729fc018576a4667c62f035cb0
Darren-Haynes <darrenhaynes@zoho.com>
2017-12-20 10:31:53 -0800

    no longer directly opening setup.py, we get it from

--- write_me/stp_info.py
+++ write_me/stp_info.py
@@ -1,53 +1,69 @@
 """Transform setup.py into a python dictionary."""
 
 import ast
+from .list_files import get_setup_file
 
 setup_parsed = {}
 
-setup_keys = ['name', 'version', 'description', 'author_email', 'url', 'packages', 'author=']
+setup_keys = ['name',
+              'version',
+              'description',
+              'author_email',
+              'url',
+              'packages',
+              'author=']
 
 
-with open('../setup.py', 'r') as sf:
-    # evaluated = ast.literal_eval(sf.read())
-    create_list = []
-    appending = False
-    for line in sf:
-        line = line.strip()
-        line = line.rstrip(',')
-        if not appending:
-            for key in setup_keys:
-                if line.startswith(key):
-                    k, v = line.split('=')
-                    if v.startswith('['):
-                        if v.endswith(']'):
-                            # import pdb; pdb.set_trace()
-                            v = ast.literal_eval(v)
-                            setup_parsed[k] = v
-                            continue
+def parse_setup_py():
+    """Covert needed info from setup.py into dict."""
+    setup_files = get_setup_file()
+    if not setup_files:
+        raise FileNotFoundError("No setup.py file found in root directory")
+
+    with open(setup_files[0], 'r') as sf:
+        create_list = []
+        appending = False
+        for line in sf:
+            line = line.strip()
+            line = line.rstrip(',')
+            if not appending:
+                for key in setup_keys:
+                    if line.startswith(key):
+                        k, v = line.split('=')
+                        if v.startswith('['):
+                            if v.endswith(']'):
+                                # import pdb; pdb.set_trace()
+                                v = ast.literal_eval(v)
+                                setup_parsed[k] = v
+                                continue
+                            else:
+                                appending = True
+                                v = v.lstrip('[')
+                                create_list.append(v.strip("'"))
+                                continue
                         else:
-                            appending = True
-                            v = v.lstrip('[')
-                            create_list.append(v.strip("'"))
+                            setup_parsed[k] = v.strip("'")
                             continue
                     else:
-                        setup_parsed[k] = v.strip("'")
                         continue
-                else:
-                    continue
 
-        else:
-            if line.endswith(']'):
-                appending = False
-                line = line.rstrip(']')
-                create_list.append(line.strip("'"))
-                if key == "author=":
-                    key = key.replace("=", "")
-                setup_parsed[key] = create_list
             else:
-                create_list.append(line.strip("'"))
+                if line.endswith(']'):
+                    appending = False
+                    line = line.rstrip(']')
+                    create_list.append(line.strip("'"))
+                    if key == "author=":
+                        key = key.replace("=", "")
+                    setup_parsed[key] = create_list
+                else:
+                    create_list.append(line.strip("'"))
+
+    if 'packages' in setup_parsed:
+        if setup_parsed['packages'] == 'find_packages()':
+            setup_parsed['packages'] = ''
+
+    return setup_parsed
 
 
-for keys in setup_parsed:
-    print(keys, setup_parsed[keys])
-
-print(type(setup_parsed['packages']))
+if __name__ == '__main__':
+    parse_setup_py()
