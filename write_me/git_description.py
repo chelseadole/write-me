838f3328a03e42dfc41b75578fb2aae316270fe4
Matt Favoino <mattfavoino@gmail.com>
2017-12-21 12:28:31 -0800

    try except fix for git description

--- write_me/git_description.py
+++ write_me/git_description.py
@@ -3,12 +3,15 @@
 
 def get_git_description():
     """Open .git/description and determine if description exists."""
-    with open('./.git/description', 'r') as git_desc:
-        for line in git_desc:
-            if line.startswith('Unnamed repository;'):
-                return 'YOUR PROJECT DESCRIPTION HERE'
-            else:
-                return git_desc.read()
+    try:
+        with open('./.git/description', 'r') as git_desc:
+            for line in git_desc:
+                if line.startswith('Unnamed repository;'):
+                    return 'YOUR PROJECT DESCRIPTION HERE'
+                else:
+                    return git_desc.read()
+    except FileNotFoundError:
+        return 'YOUR PROJECT DESCRIPTION HERE'
 
 if __name__ == '__main__':  # pragma no cover
     print(get_git_description())
